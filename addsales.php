0038f77f7799d2c5117ec30c17818164f1f0b7ce
DestinysRose <Vixen_Minamino@hotmail.com>
2016-10-04 00:06:45 +0800

    New format / style + Sales page layout
    Functions not yet included;  design was revised and updated.

---
+++ addsales.php
@@ -0,0 +1,39 @@
+﻿<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<Title>Add Sales</Title>
+</head>
+<body>
+
+<script> <!--Modify script to include items sold. Place in separate .js file in future, close both popups-->
+function myFunction() {
+    var myWindow = window.open("Success", "MsgWindow", "width=200,height=100");
+    myWindow.document.write("<p>You have successfully added the transaction.</p>");
+}
+</script>
+
+<form action = "salesview.php" method = "post">
+<h2 class = "but1"><input class = "baton2" type = "submit" name = "listall" value = "List All"></h1>
+</form>	
+	<h1> Add new sales transaction </h1>
+	<hr />
+	<form action="sale.php" method="get" name="login" onsubmit="myFunction(); return false;">
+	  Customer Name:<br>
+	  <input type="text" name="">
+	  <br><br>
+	  Date:<br>
+	  <input type="date" name="">
+	  <br><br>
+	  Item 1: 
+		<select>
+		  <option value="1">1</option>
+		  <option value="2">2</option>
+		  <option value="3">3</option>
+		  <option value="4">4</option>
+		</select>
+	  <button class="add"> Add more </button> <!--Add Javascript here to add more items-->
+	  <br><br>
+	  <input type="submit" value="Add Sales" class="submit">
+	</form>
+</body>
+</html>
---
+++ sales.php
@@ -0,0 +1,50 @@
+﻿<DOCTYPE!>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<Title>Sales Module</Title>
+</head>
+<body>
+
+<div class="top">
+	<img></img>Welcome, &lt;User&gt; <!--Have php check if used is logged in-->
+	<img></img>&lt;Date&gt; <!--Use .js get today's date -->
+	<img></img>&lt;Log Out&gt; <!--function required-->
+</div>
+<div class="side">
+	<ul id="side">
+		<li><a href="index.php">Dashboard</a></li>
+		<li><a href="sale.php">Sales</a></li>
+		<li><a href="item.php">Inventory</a></li>
+		<li><a href="item.php">Customers</a></li>
+		<li><a href="item.php">Suppliers</a></li>
+		<li><a href="item.php">Sales Report</a></li>
+		<li><a href="#" class="last">Contact</a></li>
+	</ul>
+</div>
+<div class="Body"> 
+	<h1> Sales </h1>
+	<hr />
+	<br><br>
+	<table>
+		<tr>
+			<th> Customer Name </th>
+			<th> Date </th>
+			<th> Sales ID </th>
+		</tr>
+		<tr> <!--Append database data, need to modify to allow for selecting-->
+			<td>ENTER CUSTOMER NAME HERE</td>
+			<td>ENTER DATE HERE</td>
+			<td>SALES ID</td>
+		</tr>	
+	</table>
+	<br>
+	<br>
+	<button class="add"> Add new sale </button> <!--Add Javascript here to open a popup window addsales.php->
+	<button class="view"> View selected transaction </button> <!--Add Javascript here to add more items-->
+</div>
+<div class="btm">
+ &lt; Footer &gt; <!--Change to include student IDs?-->
+</div>
+</body>
+</html>
